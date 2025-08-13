b974d6a2122cc62006018ce6cd3340b7aa51b405
Nao1a <naolme231@gmail.com>
2025-08-13 08:43:07 +0300

    g

---
+++ backend/models/UserModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, minlength: 6 },
+    password: { type: String, required: true, minlength: 8 },
+    email : {type : String, required: true, unique: true},
+    isVerified: { type: Boolean, default: false },
+    role: {
+        type : String,
+        enum : ['user', 'moderator', 'admin' , 'taxiOwner'],
+        default : 'user'
+    },
+    reputation : {type : Number, default : 5}
+}, { timestamps : true })
+
+module.exports = mongoose.model('User', UserSchema)
